9ba0e8f0156c6727372f39c9a2092468db87841f
Fadal EL ABDIOUI <elabdioui.fadal@gmail.com>
2020-08-24 14:58:33 +0200

    Model + Controller favorites

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'messages/create'
   devise_for :users
   root to: 'pages#home'
   resources :chatrooms, only: [:index, :show] do
     resources :messages, only: :create
   end
+  get '/users/:id/favorites', to: 'favorites#index', as: 'favorites_users'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_112615) do
+ActiveRecord::Schema.define(version: 2020_08_24_123511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_08_24_112615) do
   create_table "chatrooms", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -42,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_08_24_112615) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "users"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
 end
