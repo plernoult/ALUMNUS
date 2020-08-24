da042b394bc619cc4fccba1479bf346377aeac43
Francois Sommella <francois.sommella@gmail.com>
2020-08-24 16:16:37 +0200

    debug

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -4,14 +4,10 @@ const initChatroomCable = () => {
   const messagesContainer = document.getElementById('messages');
   if (messagesContainer) {
     const id = messagesContainer.dataset.chatroomId;
-
-    consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
-      received(data) {
         consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
           received(data) {
             messagesContainer.insertAdjacentHTML('beforeend', data);
-          }
-        }); // called when data is broadcast in the cable
+       // called when data is broadcast in the cable
       },
     });
   }
