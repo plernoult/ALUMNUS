0117d6f0c9363d1313dd1fb685e4a598d2525720
Fadal EL ABDIOUI <elabdioui.fadal@gmail.com>
2020-08-25 15:11:53 +0200

    Fonctionnalité ajout favoris (sans unicité des favoris pour le moment)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do 
+    resources :favorites, only: [:index, :create]
+  end
   root to: 'pages#home'
   resources :chatrooms, only: [:new, :index, :show] do
     collection do
@@ -9,6 +11,7 @@ Rails.application.routes.draw do
     resources :messages, only: :create
 
   end
-  get '/users/:id/favorites', to: 'favorites#index', as: 'favorites_users'
+  # get '/users/:id/favorites', to: 'favorites#index', as: 'favorites_users'
+  # post '/users/favorites', to: 'favorites#create', as: 'favorites_users'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
