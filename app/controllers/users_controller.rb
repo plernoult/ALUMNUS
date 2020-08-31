22aa69fa138655e90ed7b547049a33235d2e77e4
Fadal EL ABDIOUI <elabdioui.fadal@gmail.com>
2020-08-31 09:46:27 +0200

    Fonction recherche sur une page séparée + ajout critère company et changement mise en forme cards index et search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
   def index
-    @users_all = User.where.not(id: current_user.id)
     @current_city = current_user.current_city
     @prox_users = User.near(@current_city , 10).where.not(id: current_user)
+  end
 
+
+  def show
+    @user = User.find(params[:id])
+    @chatroom = Chatroom.joins(:messages).joins(:users).where("users.id = ? OR users.id = ?", @user, current_user).uniq
+  end
+
+  def search
     if params[:query].present?
       sql_query = "first_name ILIKE :query \
        OR last_name ILIKE :query \
@@ -13,21 +20,19 @@ class UsersController < ApplicationController
        OR concat(last_name,first_name) ILIKE :query \
        OR current_city ILIKE :query \
        OR occupation ILIKE :query \
+       OR company_name ILIKE :query \
        OR batch_number = :query_int"
 
         @users = User.where(sql_query, query: "%#{params[:query]}%", query_int: params[:query].to_i)
 
+
     else
       @users = User.where.not(id: current_user.id)
     end
-
   end
 
 
-  def show
-    @user = User.find(params[:id])
-    @chatroom = Chatroom.joins(:messages).joins(:users).where("users.id = ? OR users.id = ?", @user, current_user).uniq
-  end
+private
 
   def user_params
     params.require(:user).permit(:photo, :favorite, :bio)
