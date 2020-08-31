cba686edc39b67c2986ac43c65312180213edc64
Fadal EL ABDIOUI <elabdioui.fadal@gmail.com>
2020-08-31 10:00:33 +0200

    redirection profile après édition

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
     def after_update_path_for(resource)
-      user_path(resource)
+      user_profile_path
     end
     
 end
