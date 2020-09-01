6c760c4c8be802e6b29e5c24bd3d4bf16bb30712
Paul-Louis <67895372+plernoult@users.noreply.github.com>
2020-09-01 14:35:08 +0200

    Revert "test geoloc"

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable
   has_one_attached :photo
   validates :photo, presence: true
   has_many :messages
   has_many :chatrooms, through: :messages, dependent: :destroy
-  geocoded_by :current_sign_in_ip, latitude: :latitude, longitude: :longitude
-  after_save :geocode #, if: :will_save_change_to_current_city?
+  geocoded_by :current_city
+  after_validation :geocode, if: :will_save_change_to_current_city?
   has_many :favorites, dependent: :destroy
 end
