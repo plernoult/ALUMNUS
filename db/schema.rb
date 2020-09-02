223119dca2ea51737fd3318ad4a82bc79466a5df
Francois Sommella <francois.sommella@gmail.com>
2020-09-02 11:10:46 +0200

    commitschema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_142216) do
+ActiveRecord::Schema.define(version: 2020_09_01_084927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_142216) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "viewed"
     t.boolean "receiver_viewed"
     t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
@@ -84,6 +85,11 @@ ActiveRecord::Schema.define(version: 2020_08_31_142216) do
     t.float "longitude"
     t.string "bio"
     t.datetime "last_seen_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
